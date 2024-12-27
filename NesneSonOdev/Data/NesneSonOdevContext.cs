c4aa9e83d2fb47ebcf5282130435ec90939f7749
Alperen Şensoy <Alperen ensoy@BILGISAYAR>
2024-12-27 11:45:13 +0300

    Add project files.

---
+++ NesneSonOdev/Data/NesneSonOdevContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NesneSonOdev.Models;
+
+namespace NesneSonOdev.Data
+{
+    public class NesneSonOdevContext : DbContext
+    {
+        public NesneSonOdevContext (DbContextOptions<NesneSonOdevContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NesneSonOdev.Models.Customer> Customer { get; set; } = default!;
+        public DbSet<NesneSonOdev.Models.Order> Order { get; set; } = default!;
+        public DbSet<NesneSonOdev.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ NesneSonOdev/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace NesneSonOdev.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
